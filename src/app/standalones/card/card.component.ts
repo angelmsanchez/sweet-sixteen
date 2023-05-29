ab0958db06a18377ef9ef5ff5b9be47a0d2e66e2
Angel Muñoz <angelmsanchez85@gmail.com>
2023-05-29 14:59:30 +0200

    use signal to change state component

--- src/app/standalones/card/card.component.ts
+++ src/app/standalones/card/card.component.ts
@@ -1,11 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, computed, effect, signal } from '@angular/core';
 
 @Component({
   selector: 'app-card',
   standalone: true,
   templateUrl: './card.component.html',
-  styleUrls: ['./card.component.scss']
+  styleUrls: ['./card.component.scss'],
 })
 export class CardComponent {
+  firstName = signal('Jane');
+  lastName = signal('Doe');
+  fullName = computed(() => `${this.firstName()} ${this.lastName()}`);
 
+  constructor() {
+    effect(() => {
+      console.log('Name changed:', this.fullName());
+    });
+  }
+
+  setName(newName: string) {
+    this.firstName.set(newName);
+  }
 }
--- src/app/standalones/panel/panel.component.ts
+++ src/app/standalones/panel/panel.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
+import { toObservable, toSignal } from '@angular/core/rxjs-interop';
+
 import { CardComponent } from '../card/card.component';
 
 @Component({
@@ -8,4 +10,16 @@ import { CardComponent } from '../card/card.component';
   templateUrl: './panel.component.html',
   styleUrls: ['./panel.component.scss'],
 })
-export class PanelComponent {}
+export class PanelComponent {
+  count = signal(0);
+  count$ = toObservable(this.count);
+  countSignal = toSignal(this.count$);
+
+  ngOnInit() {
+    this.count$.subscribe(() => {
+      console.log('Count changed:', this.count());
+    });
+  }
+}
+
+
